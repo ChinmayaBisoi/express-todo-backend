c1af1c9c9ac1476b7895bf6bd10034f9813f53fd
C_bi__ <chinmaya@kloudle.com>
2023-09-14 13:26:42 +0530

    test

--- config/config.js
+++ config/config.js
@@ -1,10 +1,23 @@
 require("dotenv").config();
 const corsOptions = require("./corsOptions");
 
+// Define your allowed origins
+const allowedOrigins = [
+  "https://cbi-todo-app.vercel.app",
+  // Add other allowed origins as needed
+];
+
 const config = {
   port: process.env.PORT || 3500,
   corsOptions: corsOptions,
   databaseUri: process.env.DATABASE_URI || "",
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
 
 module.exports = config;
