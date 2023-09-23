e71f3587fef775fbfdc85c2c623a9b049cd5d20c
C_bi__ <chinmaya@kloudle.com>
2023-09-23 22:33:17 +0530

    sep23 22.33

--- app.js
+++ app.js
@@ -16,6 +16,23 @@ app.use(cors(config.corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  // Set allowed headers
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  // Set allowed methods
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+  // Set the 'Access-Control-Allow-Credentials' header to 'true'
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  // Continue to the next middleware
+  next();
+});
+
 app.get("/", (req, res) => {
   console.log("working properly");
   res.send("hello there");
