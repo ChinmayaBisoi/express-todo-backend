3ffc070b1e7afc9d0d0c8c8c3a04a5217c387403
C_bi__ <chinmaya@kloudle.com>
2023-09-23 22:09:43 +0530

    added credential sep23 10.10

--- config/config.js
+++ config/config.js
@@ -11,12 +11,18 @@ const config = {
   port: process.env.PORT || 8080,
   corsOptions: corsOptions,
   databaseUri: process.env.DATABASE_URI || "",
+  credentials: true,
   origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg =
+        "The CORS policy for this site does not " +
+        "allow access from the specified Origin.";
+      return callback(new Error(msg), false);
     }
+    return callback(null, true);
   },
 };
 
